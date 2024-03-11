00853dc878a4f116b2012a85ad3888e336cfc204
Yu Zhou Zhang <yuzspanish@gmail.com>
2024-03-11 03:02:04 +0000

    update template file

--- dist/bundle.js
+++ dist/bundle.js
@@ -116,7 +116,7 @@ eval("\n\n/* istanbul ignore next  */\nfunction styleTagTransform(css, styleElem
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n\n\nconst contentContainer = document.getElementById(\"content\");\nconst testContent = document.createElement(\"p\");\ntestContent.innerHTML = \"Hello from Webpack!\";\ncontentContainer.appendChild(testContent);\n\n\n//# sourceURL=webpack://webpack-template/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n\nvar contentContainer = document.getElementById(\"content\");\nvar testContent = document.createElement(\"p\");\ntestContent.innerHTML = \"Hello from Webpack!\";\ncontentContainer.appendChild(testContent);\n\n//# sourceURL=webpack://webpack-template/./src/index.js?");
 
 /***/ })
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,27 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
+  mode: 'development',
+  entry: './src/index.js',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        // use: ["file-loader"],
-        type: "asset/resource",
+        use: ['file-loader'],
+        type: 'asset/resource',
       },
     ],
   },
